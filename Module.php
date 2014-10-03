fa47d8731442960acc4518cc58034183e0f96c33
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2014-10-03 22:43:22 +0100

    POC ZF2 Module Listener for Codeception C3 remote code coverage

---
+++ Module.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CodeceptionZf2CodeCoverage;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * Class Module
+ * @package CodeceptionZf2CodeCoverage
+ */
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+
+        $eventManager->attach(
+            new C3()
+        );
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
---
+++ src/CodeceptionZf2CodeCoverage/Listener/C3.php
@@ -0,0 +1,88 @@
+<?php
+namespace CodeceptionZf2CodeCoverage;
+
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\EventInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Stdlib\CallbackHandler;
+
+/**
+ * Class C3
+ *
+ * @package CodeceptionZf2CodeCoverage
+ */
+class C3 implements ListenerAggregateInterface
+{
+
+    /**
+     * @var array
+     */
+    protected $listeners = array();
+
+    /**
+     * @return array
+     */
+    public function getListeners()
+    {
+        return $this->listeners;
+    }
+
+    /**
+     * @param CallbackHandler $listeners
+     *
+     * @return C3
+     */
+    public function addListener(CallbackHandler $listeners)
+    {
+        $this->listeners[] = $listeners;
+
+        return $this;
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return bool
+     */
+    public function removeListener($index)
+    {
+        if (!empty($this->listeners[$index])) {
+            unset($this->listeners[$index]);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param EventManagerInterface $events
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->addListener($events->attach(MvcEvent::EVENT_ROUTE, array($this, 'codeCoverage')));
+    }
+
+    /**
+     * @param EventManagerInterface $events
+     */
+    public function detach(EventManagerInterface $events)
+    {
+        foreach ($this->getListeners() as $index => $listener) {
+            if ($events->detach($listener)) {
+                $this->removeListener($index);
+            }
+        }
+    }
+
+    /**
+     * @param EventInterface $event
+     */
+    public function codeCoverage(EventInterface $event)
+    {
+        if ('development' === getenv('APP_ENV')) {
+            include 'vendor/codeception/c3/c3.php';
+        }
+    }
+}
